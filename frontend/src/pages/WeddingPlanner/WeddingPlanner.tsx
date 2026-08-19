abeaa38798e7eea09b26555fc814fd74a7d0ebd7
Harsha Royal <iharsharoyal@gmail.com>
2026-08-19 15:16:06 +0530

    Fix import path for CustomDateTimePicker

--- frontend/src/pages/WeddingPlanner/WeddingPlanner.tsx
+++ frontend/src/pages/WeddingPlanner/WeddingPlanner.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import api from '../../services/api';
-import CustomDateTimePicker from '../../components/Shared/CustomDateTimePicker';
+import CustomDateTimePicker from '../../components/CustomDateTimePicker';
 
 interface Expense {
   id: number;
