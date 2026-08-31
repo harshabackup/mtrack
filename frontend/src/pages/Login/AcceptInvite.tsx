38818e86ebd749fb06a24667b9592eeb3a11ff2a
Harsha Royal <iharsharoyal@gmail.com>
2026-08-31 14:39:55 +0530

    Update AcceptInvite

--- frontend/src/pages/Login/AcceptInvite.tsx
+++ frontend/src/pages/Login/AcceptInvite.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import api from '../../services/api';
-import { useAuth } from '../../context/AuthContext';
 
 const AcceptInvite: React.FC = () => {
   const [searchParams] = useSearchParams();
